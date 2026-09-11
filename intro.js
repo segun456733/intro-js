5b3074a671c427e2a21d35f5cf32133d909c87b0
Segun Emmanuel <segunsmart03@gmail.com>
2026-09-11 13:32:00 +0100

    Initial commit my info

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "segun"
+const age = 24
+const favoriteProgrammingLanguage = "JavaScript"
+console.log("Name: " + name)
+console.log("Age: " + age)
+console.log("My favorite programming language is: " + favoriteProgrammingLanguage )
