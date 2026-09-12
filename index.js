1f26c5f13b28ed5e162ac3add37463b28e67b6af
Segun Emmanuel <segunsmart03@gmail.com>
2026-09-12 22:37:49 +0100

    initial commit my good project

---
+++ index.js
@@ -0,0 +1,4 @@
+const userFirstName = "Smart Dave"
+const usercity = "Lagos"
+const userCountry = "Nigerian"
+console.log("My name is"+ " " + userFirstName + " i in " + usercity + " i am a " + userCountry)
